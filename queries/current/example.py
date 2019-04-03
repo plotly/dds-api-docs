47980bbbd211cf4fa01d7ad6bcc65a2280638852
Mike Fix <mrfix84@gmail.com>
2019-04-03 13:16:41 -0700

    add isAdmin to 'current' query

--- queries/current/example.py
+++ queries/current/example.py
@@ -6,6 +6,7 @@ current_query = gql(
     {
         current {
             username
+            isAdmin
         }
     }
     """
@@ -14,3 +15,4 @@ current_query = gql(
 current = dds_client.execute(current_query)
 
 print("username: {current.username}")
+print("is admin user: {current.isAdmin}")
