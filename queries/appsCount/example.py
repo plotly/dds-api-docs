81a1bf87cb6196d30ea816347eb41881a8b414be
Mike Fix <mrfix84@gmail.com>
2019-04-03 14:39:55 -0700

    add 'appsCount' example.py

---
+++ queries/appsCount/example.py
@@ -0,0 +1,18 @@
+import gql
+import dds_client
+
+apps_count_query = gql(
+    """
+    {
+        appsCount {
+            username
+            count
+        }
+    }
+    """
+)
+
+result = dds_client.execute(apps_count_query)
+
+print(f"first count: {result[0].count}")
+print(f"first username: {result[0].username}")
