fd76f14fd6e827dcb8a344ba039fc358e3f1cc36
Mike Fix <mrfix84@gmail.com>
2019-04-03 16:11:25 -0700

    add fetch subsequent page example to apps by_page example

--- queries/apps/by_page.py
+++ queries/apps/by_page.py
@@ -3,8 +3,8 @@ import dds_client
 
 apps_page_query = gql(
     """
-    {
-        apps(page: 2) {
+    query AppsQuery ($page: Int!) {
+        apps(page: $page) {
             apps {
                 urlOnServer
                 metadata {
@@ -17,8 +17,14 @@ apps_page_query = gql(
     """
 )
 
-result = dds_client.execute(apps_page_query)["apps"]
+page = 2
+
+result = dds_client.execute(apps_page_query, {"page": page})["apps"]
 
 print(f"next page: {result['nextPage']}")
 print(f"app url on server: {result['apps'][0]['urlOnServer']}")
 print(f"app show in portal: {result['apps'][0]['metadata']['showInPortal']}")
+
+nextPage = result["nextPage"]
+
+third_page = dds_client.execute(apps_page_query, {"page": nextPage})["apps"]
