f33432f2355ec1b1bb136ae1fcb8e3b80453d911
Mike Fix <mrfix84@gmail.com>
2019-04-03 13:47:28 -0700

    undo apps query typo

--- queries/apps/by_page.py
+++ queries/apps/by_page.py
@@ -21,6 +21,6 @@ apps_page_query = gql(
 
 result = dds_client.execute(apps_page_query)
 
-print(f"next page: {result.apps.nextPage}")
-print(f"app url on server: {result.apps.apps[0].urlOnServer}")
-print(f"app show in portal: {result.apps.apps[0].metadata.showInPortal}")
+print(f"next page: {result.nextPage}")
+print(f"app url on server: {result.apps[0].urlOnServer}")
+print(f"app show in portal: {result.apps[0].metadata.showInPortal}")
