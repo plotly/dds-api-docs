38f4e32f78a348a601dde137c7351c42d21f5518
Mike Fix <mrfix84@gmail.com>
2019-04-04 11:02:13 -0700

    upload logos, use adjacent file paths for examples

--- mutations/uploadPortalLogo/example.py
+++ mutations/uploadPortalLogo/example.py
@@ -1,9 +1,14 @@
+import os
 import json
 import requests
 from dds import DDS_ENDPOINT
 
 logo_file_name = "logo.png"
-logo_content = open(logo_file_name, "rb")
+logo_location = os.path.join(
+    os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))),
+    logo_file_name,
+)
+logo_content = open(logo_location, "rb")
 
 mutation = """
 mutation($logo: Upload!){
